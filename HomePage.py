ecd34cf6dc808061bffe93f3d3ed6e602c5c3092
shahid <shahid.biitmcs@gmail.com>
2023-09-17 02:35:19 -0700

    first commit

---
+++ HomePage.py
@@ -0,0 +1,11 @@
+from selenium.webdriver.common.by import By
+
+
+class HomePage:
+    def __def__(self, driver):
+        self.driver = driver
+
+    shop = (By.XPATH, "//a[normalize-space()='Shop'])[1]")
+
+    def shopItems(self):
+        return self.driver.find_element(*HomePage.shop)
